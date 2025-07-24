8fdc2c168fea930568590ed8e5de26dec1cb8067
rg861348 <rg861348@gmail.com>
2025-07-24 12:06:31 +0530

    Create Exp_1_1_hello world.cpp

---
+++ Exp_1_1_hello world.cpp
@@ -0,0 +1,18 @@
+//RISHITA GOSWAMI 
+//24070123086
+//B1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World!";
+
+    return 0;
+}
+
+/*
+output:
+Hello World!
+*/
